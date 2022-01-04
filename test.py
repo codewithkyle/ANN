3f159981f022e8b3fe49b0ba6084d11182490957
Kyle Andrews <codingwithkyle@gmail.com>
2022-01-04 15:30:51 -0500

    ✨ Updated test to include 10 round of training data

--- test.py
+++ test.py
@@ -10,27 +10,28 @@ loss_activation = Softmax_CategoricalCrossEntropy()
 optimizer = SGD_Optimizer()
 
 # Create test data
-X, y = create_data(100, 3)
+for d in range(10):
+    X, y = create_data(100, 3)
+    for epoch in range(10001):
+        # Do work
+        layer1.forward(X)
+        act1.forward(layer1.output)
+        layer2.forward(act1.output)
+        loss = loss_activation.forward(layer2.output, y)
+        accuracy = accuracy_func.calculate(loss_activation.output, y)
+        
+        if not epoch % 100:
+            print(  f'set: {d}, ' + 
+                    f'epoch: {epoch}, ' +
+                    f'acc: {accuracy:.3f}, ' +
+                    f'loss: {loss:.3f}')
 
-for epoch in range(10001):
-    # Do work
-    layer1.forward(X)
-    act1.forward(layer1.output)
-    layer2.forward(act1.output)
-    loss = loss_activation.forward(layer2.output, y)
-    accuracy = accuracy_func.calculate(loss_activation.output, y)
-    
-    if not epoch % 100:
-        print(  f'epoch: {epoch}, ' +
-                f'acc: {accuracy:.3f}, ' +
-                f'loss: {loss:.3f}')
+        # Backpropagation
+        loss_activation.backward(loss_activation.output, y)
+        layer2.backward(loss_activation.dinputs)
+        act1.backward(layer2.dinputs)
+        layer1.backward(act1.dinputs)
 
-    # Backpropagation
-    loss_activation.backward(loss_activation.output, y)
-    layer2.backward(loss_activation.dinputs)
-    act1.backward(layer2.dinputs)
-    layer1.backward(act1.dinputs)
-
-    # Optimize
-    optimizer.update(layer1)
-    optimizer.update(layer2)
+        # Optimize
+        optimizer.update(layer1)
+        optimizer.update(layer2)
